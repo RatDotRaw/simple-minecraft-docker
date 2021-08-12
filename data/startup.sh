3a3da7c08b6b86461169dfb5baa05db742fcf019
Rat <staf.dierickx@gmail.com>
2021-08-12 16:11:07 +0200

    made server detection more robust.

--- data/startup.sh
+++ data/startup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+source config
 
 if [ -f "/home/container/minecraft/start.sh" ]; then
-    echo "+=+ Executing start.sh."
-    bash /home/container/minecraft/start.sh
-else
-    echo "+=+ start.sh not found, executing setup.sh."
-    bash /home/container/data/setup.sh
+    if [ -f "/home/container/minecraft/$jar_name" ]; then
+        echo "+=+ Executing start.sh."
+        bash /home/container/minecraft/start.sh
+    else
+        echo "+=+ server not found, executing setup.sh."
+        bash /home/container/data/setup.sh
+    fi
 fi
