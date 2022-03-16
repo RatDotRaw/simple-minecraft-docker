42ba316e8868523b6b9021cc8e1af8d8847ab871
Rat <staf.dierickx@gmail.com>
2022-03-16 20:13:52 +0100

    rcon

--- minecraft-server/rcon/rcon.js
+++ minecraft-server/rcon/rcon.js
@@ -1,37 +1,49 @@
 var Rcon = require("rcon");
 
-const host = 'localhost'
-const port = '25575'
-const password = '"changeme"'
+const host = "localhost";
+const port = "25575";
+const password = '"changeme"';
 
 var conn = new Rcon(host, port, password);
 
 //args to string
 var argumentString = "";
-var args = process.argv.splice(process.execArgv.length + 2)
+var args = process.argv.splice(process.execArgv.length + 2);
 for (var i = 0; i < args.length; i++) {
-  if (i === args.length - 1) argumentString += args[i]
-  else argumentString += args[i] + ' '
+    if (i === args.length - 1) argumentString += args[i];
+    else argumentString += args[i] + " ";
 }
 
-//check if argumentstring is empty
+//check if argument is empty
 if (argumentString.length < 1) {
-    console.log("RconApp::Error: Please specify an RCON command");
-    process.exit();
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
 
 console.log("RconApp::Relaying RCON command: " + argumentString);
 
 conn.on("auth", function () {
-    console.log("connected!");
-    conn.send(argumentString)
-}).on("response", function (str) {
-    console.log("Response: " + str);
-}).on("error", function (err) {
-    console.log("Error: " + err);
-}).on("end", function () {
-    console.log("Connection closed");
+    console.log("rcon::connected!");
+    console.log("rcon::sending: " + argumentString);
+    conn.send(argumentString);
+
+    console.log("rcon::connection will close in 10 seconds!");
+    sleep(10000);
     process.exit();
-});
+})
+    .on("response", function (str) {
+        console.log("rcon::Response: " + str);
+    })
+    .on("error", function (err) {
+        console.log("rcon::Error: " + err);
+    })
+    .on("end", function () {
+        console.log("rcon::Connection closed");
+        process.exit();
+    });
 
 conn.connect();
